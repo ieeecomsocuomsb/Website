5b08b6c9b5dd7cbfb80f5c7571bbad3348e7b9f8
krish-anu <krishnaanu200302@gmail.com>
2025-12-09 18:22:56 +0530

    style:Allignment adjusted for carousel

--- comsoc-website/src/components/Carousel.tsx
+++ comsoc-website/src/components/Carousel.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-
 import AGM1 from "../assets/AGM_images/AGM-1.jpeg";
 import AGM2 from "../assets/AGM_images/AGM-2.jpeg";
 import AGM3 from "../assets/AGM_images/AGM-3.jpeg";
-import AGM4 from "../assets/AGM_images/AGM-4.jpeg"  ;
+import AGM4 from "../assets/AGM_images/AGM-4.jpeg";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, EffectCoverflow, Autoplay } from "swiper/modules";
@@ -62,18 +61,162 @@ const Carousel: React.FC = () => {
             the chapter’s key activities, including ComFix and the Datacenter
             Field Visit.
           </p>
-          <br />
-          <p>
-            With the transition of responsibilities, the chapter begins a new
-            term dedicated to strengthening communication-related learning and
-            collaboration. The AGM marked both appreciation for past service and
-            renewed enthusiasm toward advancing the chapter’s role in the
-            evolving field of communication technology.
-          </p>
         </div>
 
         <Swiper
           className="myswiper w-[80%]"
+          modules={[Pagination, EffectCoverflow, Autoplay]}
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 3,
+            slideShadows: true,
+          }}
+          loop={true}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 1,
+            },
+            1024: {
+              slidesPerView: 2,
+            },
+            1560: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {slider.map((data, idx) => (
+            <SwiperSlide
+              key={idx}
+              style={{ backgroundImage: `url(${data.url})` }}
+              className="myswiper-slider"
+            >
+              <div>
+                {/* <h2>{data.title}</h2> */}
+                <p>{data.description}</p>
+                <a
+                  href={data.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="slider-btn"
+                >
+                  explore
+                </a>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="flex gap-8 w-[90%]">
+        <Swiper
+          className="myswiper w-full h-full"
+          modules={[Pagination, EffectCoverflow, Autoplay]}
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 3,
+            slideShadows: true,
+          }}
+          loop={true}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 1,
+            },
+            1024: {
+              slidesPerView: 2,
+            },
+            1560: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {slider.map((data, idx) => (
+            <SwiperSlide
+              key={idx}
+              style={{ backgroundImage: `url(${data.url})` }}
+              className="myswiper-slider"
+            >
+              <div>
+                <h2>{data.title}</h2>
+                <p>{data.description}</p>
+                <a
+                  href={data.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="slider-btn"
+                >
+                  explore
+                </a>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="carousel-content w-[40%] p-6">
+          <h1>Comfix</h1>
+          <hr />
+          <p>
+            Communication continues to shape the modern world through its
+            influence on connectivity, information flow, and emerging
+            technologies. In this evolving landscape, Comfix’25 provided
+            undergraduates a space to explore real communication challenges and
+            develop innovative ideas that strengthen future communication
+            systems. Organized by the IEEE Communications Society Student Branch
+            Chapter of the University of Moratuwa, the event fostered analytical
+            thinking and creative problem-solving.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex gap-8 w-[90%]">
+        <div className="carousel-content w-[40%] p-6">
+          {/* <span>discover</span> */}
+          <h1>Field Visit</h1>
+          <hr />
+          <p>
+            Modern communication relies heavily on resilient digital
+            infrastructure, and datacenters stand at the core of this global
+            connectivity. To help undergraduates gain exposure to these
+            essential systems, the IEEE Communications Society Student Branch
+            Chapter of the University of Moratuwa organized a field visit to a
+            national-level datacenter, offering a first-hand look at how uptime,
+            reliability, and secure data flow are maintained.
+          </p>
+          <br />
+          <p>
+            The visit allowed students to observe the critical engineering
+            behind communication networks that support daily digital
+            interactions. This experience strengthened their understanding of
+            how robust communication architectures sustain the increasingly
+            interconnected world around us.
+          </p>
+        </div>
+
+        <Swiper
+          className="myswiper w-full h-full"
           modules={[Pagination, EffectCoverflow, Autoplay]}
           effect={"coverflow"}
           grabCursor={true}
@@ -128,155 +271,6 @@ const Carousel: React.FC = () => {
           ))}
         </Swiper>
       </div>
-      {/* <img src={bgdonut1} alt="bg image" className="bgdonut1" />
-      <img src={bgdonut2} alt="bg image" className="bgdonut2" /> */}
-      <div className="flex gap-8 w-[90%]">
-        <div className="carousel-content w-[40%] p-6">
-          {/* <span>discover</span> */}
-          <h1>Comfix</h1>
-          <hr />
-          <p>
-            Communication continues to shape the modern world through its
-            influence on connectivity, information flow, and emerging
-            technologies. In this evolving landscape, Comfix’25 provided
-            undergraduates a space to explore real communication challenges and
-            develop innovative ideas that strengthen future communication
-            systems. Organized by the IEEE Communications Society Student Branch
-            Chapter of the University of Moratuwa, the event fostered analytical
-            thinking and creative problem-solving.
-            </p><br /><p>
-            As the second iteration of this communication-focused ideathon,
-            Comfix’25 brought together enthusiastic young innovators who share a
-            passion for advancing the field. Their contributions highlight the
-            growing importance of communication-driven solutions in addressing
-            the needs of an interconnected society.
-          </p>
-        </div>
-      </div>
-
-      <Swiper
-        className="myswiper w-full h-full"
-        modules={[Pagination, EffectCoverflow, Autoplay]}
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 3,
-          slideShadows: true,
-        }}
-        loop={true}
-        pagination={{ clickable: true }}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 1,
-          },
-          1024: {
-            slidesPerView: 2,
-          },
-          1560: {
-            slidesPerView: 3,
-          },
-        }}
-      >
-        {slider.map((data, idx) => (
-          <SwiperSlide
-            key={idx}
-            style={{ backgroundImage: `url(${data.url})` }}
-            className="myswiper-slider"
-          >
-            <div>
-              <h2>{data.title}</h2>
-              <p>{data.description}</p>
-              <a
-                href={data.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="slider-btn"
-              >
-                explore
-              </a>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <div className="flex gap-8 w-[90%]">
-        <div className="carousel-content w-[40%] p-6">
-          {/* <span>discover</span> */}
-          <h1>Field Visit</h1>
-          <hr />
-          <p>
-            Modern communication relies heavily on resilient digital infrastructure, and datacenters stand at the core of this global connectivity. To help undergraduates gain exposure to these essential systems, the IEEE Communications Society Student Branch Chapter of the University of Moratuwa organized a field visit to a national-level datacenter, offering a first-hand look at how uptime, reliability, and secure data flow are maintained.</p><br/><p>
-
-The visit allowed students to observe the critical engineering behind communication networks that support daily digital interactions. This experience strengthened their understanding of how robust communication architectures sustain the increasingly interconnected world around us.
-          </p>
-        </div>
-      </div>
-
-      <Swiper
-        className="myswiper w-full h-full"
-        modules={[Pagination, EffectCoverflow, Autoplay]}
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 3,
-          slideShadows: true,
-        }}
-        loop={true}
-        pagination={{ clickable: true }}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 1,
-          },
-          1024: {
-            slidesPerView: 2,
-          },
-          1560: {
-            slidesPerView: 3,
-          },
-        }}
-      >
-        {slider.map((data, idx) => (
-          <SwiperSlide
-            key={idx}
-            style={{ backgroundImage: `url(${data.url})` }}
-            className="myswiper-slider"
-          >
-            <div>
-              <h2>{data.title}</h2>
-              <p>{data.description}</p>
-              <a
-                href={data.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="slider-btn"
-              >
-                explore
-              </a>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
     </div>
   );
 };
